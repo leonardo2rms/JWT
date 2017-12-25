5da3549bbe8a3775514f1b1b70191f17b22c4eba
tnotb <jleodosramos@gmail.com>
2017-12-25 18:53:42 -0300

    initial commit

---
+++ src/main/java/org/somos/SomosApplication.java
@@ -0,0 +1,12 @@
+package org.somos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomosApplication.class, args);
+	}
+}
